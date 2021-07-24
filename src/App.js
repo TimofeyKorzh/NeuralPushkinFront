8357532390bbb04668152cbd42d03b455fcad085
MyLittleCoin <mylittlecoin1@gmail.com>
2021-07-24 03:39:05 +0300

    fixed design

--- src/App.js
+++ src/App.js
@@ -83,14 +83,16 @@ function App() {
       
       
     <form noValidate autoComplete='off'>
+    <div className='centering'></div>
       <h1>Neural Pushkin</h1>
       <img src="npushkin.jpg" width="250" height="250" alt="Neural Pushkin"></img>
+      
       <p>Мы вышли в стадию открытого бета-тестирования!<br></br> Начните писать ниже, а нейросеть продолжит...</p>
       <p>Сбор средств на <a href="https://www.tinkoff.ru/sl/6Nv1r2nsuiQ">лучшие сервера</a> для Пушкина</p>
       <p>!ВНИМАНИЕ! Сейчас сервер находится под интенсивной нагрузкой и генерация может длиться вплоть до часа!</p>
       <Typography id="discrete-slider"  > Уровень креативности </Typography>
       <Slider
-        defaultValue={1}
+        defaultValue={0.1}
         getAriaValueText={valuetext}
         aria-labelledby="discrete-slider"
         valueLabelDisplay="auto"
@@ -99,20 +101,22 @@ function App() {
         min={0.1}
         max={2}
       />
+      <p></p>
       <TextField className='form textinput' multiline fullWidth value={text} onChange={handleChange} />
       <Button onClick={generateText} />
+      
     </form>
 
     {generatedText.pending &&
       <div className='result pending'>Подождите!</div>}
 
-        
+        <div className='centering'>
         <p>Created by <a href="https://t.me/datapron">Alex Wortega</a>, <a href="https://t.me/def_model_train">Arina Puсhkova</a></p>
         <p>Front by <a href="https://github.com/TimofeyKorzh">Timofey Korzhebin</a></p>
-        <p>Благодарим за информационную поддержку <a href="https://t.me/NeuralShit">NeuralShit</a>, <a href="https://t.me/denissexy">DenisSexyIt</a>, <a href="https://t.me/DeepFaker">DeepFake</a></p>
+        <p>Благодарим за информационную поддержку <br></br><a href="https://t.me/NeuralShit">NeuralShit</a>, <a href="https://t.me/denissexy">DenisSexyIt</a>, <a href="https://t.me/DeepFaker">DeepFake</a></p>
 
         <p>Neural Pushkin in <a href="https://twitter.com/NeuralPushkin">Twitter</a></p>
-        
+        </div>
         
   </div>
   </MuiThemeProvider>

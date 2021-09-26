90a3d8070aa8fa932c22c26e21512dc5cb5795c2
MyLittleCoin <mylittlecoin1@gmail.com>
2021-09-26 23:29:08 +0300

    fixed placement

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,15 +1,23 @@
 import { Button as MaterialButton } from '@material-ui/core';
 import React from 'react';
+import styled from 'styled-components';
 
+const Container = styled.div`
+ text-align: center;
+`;
 
 const Button = ({ onClick }) => (
+  <Container>
+            <div>
     <MaterialButton
-        style={{ marginTop: '1em', width: 'fit-content' }}
+        style={{marginTop: '1em',  width: 'auto' }}
         onClick={onClick}
         variant="outlined"
         color="primary">
         Допушкинизировать
   </MaterialButton>
+  </div>
+  </Container>
 )
 
 export default Button;

13f3d8e92bad144ae679db19039ad65eb4b4c6b9
MyLittleCoin <mylittlecoin1@gmail.com>
2021-09-28 17:34:01 +0300

    changed design

--- src/App.js
+++ src/App.js
@@ -150,13 +150,11 @@ function App() {
       
     
     <form noValidate autoComplete='off'>
-    <div className='centering'></div>
+    <div className='centering'>
       <h1>Neural Pushkin</h1>
       <img src="npushkin2.jpg" width="250" height="250" alt="Neural Pushkin"></img>
+      </div>
       
-      <p>Мы вышли в стадию открытого бета-тестирования!<br></br> Начните писать ниже, а нейросеть продолжит...</p>
-      <p>Сбор средств на <a href="https://www.tinkoff.ru/sl/6Nv1r2nsuiQ">лучшие сервера</a> для Пушкина</p>
-      <p>!ВНИМАНИЕ! Сейчас сервер находится под интенсивной нагрузкой и генерация может длиться вплоть до часа!</p>
       <Typography id="discrete-slider"  > Уровень креативности </Typography>
       <Slider
         defaultValue={0.7}
@@ -211,6 +209,7 @@ function App() {
         <p>Благодарим за информационную поддержку <br></br><a href="https://t.me/NeuralShit">NeuralShit</a>, <a href="https://t.me/denissexy">DenisSexyIt</a>, <a href="https://t.me/DeepFaker">Deepfaker</a>, <a href="https://t.me/def_model_train">Arina Puсhkova</a></p>
 
         <p>Neural Pushkin in <a href="https://twitter.com/NeuralPushkin">Twitter</a></p>
+        <p>Сбор средств на <a href="https://www.tinkoff.ru/sl/6Nv1r2nsuiQ">лучшие сервера</a> для Пушкина</p>
         </div>
         
   </div>
@@ -220,3 +219,6 @@ function App() {
 }
 
 export default App;
+/*<p>Мы вышли в стадию открытого бета-тестирования!<br></br> Начните писать ниже, а нейросеть продолжит...</p>
+      <p>Сбор средств на <a href="https://www.tinkoff.ru/sl/6Nv1r2nsuiQ">лучшие сервера</a> для Пушкина</p>
+      <p>!ВНИМАНИЕ! Сейчас сервер находится под интенсивной нагрузкой и генерация может длиться вплоть до часа!</p>*/

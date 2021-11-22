79350f241772c2efeee71c5b56d2a68813c99da3
MyLittleCoin <mylittlecoin1@gmail.com>
2021-11-23 00:57:27 +0300

    reconfig

---
+++ build/precache-manifest.082605f962474ba8b42532b18f9d7f9e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ea4297548b0ec1d1e01107e4504f8c8b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "023d0b117a1e359171c1",
+    "url": "/static/css/main.9cb7621c.chunk.css"
+  },
+  {
+    "revision": "10609d419d11716ef37d",
+    "url": "/static/js/2.491a3bdb.chunk.js"
+  },
+  {
+    "revision": "023d0b117a1e359171c1",
+    "url": "/static/js/main.d63fc378.chunk.js"
+  },
+  {
+    "revision": "3aef5e031f0c71865fde",
+    "url": "/static/js/runtime-main.bc94bbd8.js"
+  },
+  {
+    "revision": "b98f57ac686fc135914a844ec0ce8d49",
+    "url": "/static/media/MTCORSVA.b98f57ac.TTF"
+  },
+  {
+    "revision": "f4d9b5989875200a497db9dfa594b3da",
+    "url": "/static/media/Minion.f4d9b598.ttf"
+  }
+]);
